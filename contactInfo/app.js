3809dc6dc338ddfbaebe1128f5a0ddf347023bf7
lilxvg <132747304+lilxvg@users.noreply.github.com>
2023-07-01 11:23:15 +0300

    Add files via upload

--- contactInfo/app.js
+++ contactInfo/app.js
@@ -2,10 +2,17 @@ function copyToClipboard() {
     // Get the text value to copy
     var text = "LILXVG@gmail.com";
     var copDiv = document.getElementById("email");
-
+    var copDiv2 = document.getElementById("copied");
     // Create a temporary input element
     var tempInput = document.createElement("input");
-    
+    if (/Mobi|Android/i.test(navigator.userAgent)) {
+        // Code to run if user is on a phone
+        copDiv2.style.display="flex"
+        copDiv2.style.color="#f6f6f6"
+      } else {
+        // Code to run if user is not on a phone
+        copDiv2.style.display="none"
+      }
     // Set the value of the temporary input element to the text value to copy
     tempInput.setAttribute("value", text);
   
@@ -26,5 +33,6 @@ function copyToClipboard() {
     copDiv.textContent = newDiv;
     setTimeout(function() {
         copDiv.textContent = text;
+        copDiv2.style.display="none"
     }, 1500);
   }
