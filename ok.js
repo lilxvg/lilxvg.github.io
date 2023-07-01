9886176bce00a8a12ca764bd612c7737122a20d0
lilxvg <132747304+lilxvg@users.noreply.github.com>
2023-07-01 03:15:54 +0300

    Add files via upload

--- ok.js
+++ ok.js
@@ -1,7 +1,22 @@
-function scrollToSection() {
+function scrollToProjects() {
     // get the element to scroll to by its ID
     var section = document.getElementById("projects");
   
     // scroll to the element using the scrollIntoView method
     section.scrollIntoView({ behavior: "smooth" });
 }
+
+function scrollToHome() {
+    // get the element to scroll to by its ID
+    var section = document.getElementById("Home-page");
+  
+    // get the position of the section from the top of the page
+    var sectionPosition = section.getBoundingClientRect().top;
+  
+    // scroll to the element using the scrollIntoView method with an offset of 300px from the top
+    window.scroll({
+      top: sectionPosition + window.pageYOffset - 300,
+      left: 0,
+      behavior: "smooth",
+    });
+  }
